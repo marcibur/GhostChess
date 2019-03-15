8bde2b05327dd48a0810358ca6221448cfa002e8
Marcin Burak <marcin.burak@outlook.com>
2019-03-15 21:08:03 +0100

    Chess board rendered

---
+++ src/GhostChess.Web/Pages/Game.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GhostChess.Web.Pages
+{
+    [Authorize]
+    public class GameModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
